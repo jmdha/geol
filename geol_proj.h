4c6f18fb1ed142b298c0d2f78d5c4b1804c69364
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-11-06 15:45:59 +0100

    feat: split proj to deg/rad

--- geol_proj.h
+++ geol_proj.h
@@ -1,16 +1,12 @@
 #ifndef GEOL_PROJ_H
 #define GEOL_PROJ_H
 
-#define EARTH_SEMI_MAJOR_AXIS 6378137
-#define EARTH_SEMI_MINOR_AXIS 6356752
-
-void WGS84toECEF(
+void WGS84toECEF_deg(
 	double* x,   double* y,   double* z,
-	double  lat, double  lon, double  h,
-	double  a,   double  b
+	double  lat, double  lon, double  h
 );
 
-void WGS84toECEFe(
+void WGS84toECEF_rad(
 	double* x,   double* y,   double* z,
 	double  lat, double  lon, double  h
 );
@@ -19,31 +15,36 @@ void WGS84toECEFe(
 
 #include <math.h>
 
-void WGS84toECEF(
-	double* x,   double* y,  double* z,
-	double  lat, double  lon, double h,
-	double  a,   double  b
-) {
-	const double f = 1 - b / a;
-	const double E = 1 - (b * b) / (a * a);
-	const double N = a / sqrt(1 - E * (sin(lat) * sin(lat)));
-	*x = (N + h) * cos(lat) * cos(lon);
-	*y = (N + h) * cos(lat) * sin(lon);
-	*z = ((1 - f) * (1 - f) * N + h) * sin(lat);
-}
+#define _geol_pi 3.1415926535898
+#define _geol_a  6378137.0
+#define _geol_b  6356752.3142
+#define _geol_f  0.00335281066475
+#define _geol_E  0.00669437999014
 
-void WGS84toECEFe(
+#define _geol_degtorad(deg) ((deg) * _geol_pi / 180.0)
+#define _geol_radtodeg(rad) ((rad) * 180.0 / _geol_pi)
+
+void WGS84toECEF_deg(
 	double* x,   double* y,   double* z,
 	double  lat, double  lon, double  h
 ) {
-	const double a = 6378137.0;
-	const double b = 6356752.3142;
-	const double f = 1 - b / a;
-	const double E = 1 - (b * b) / (a * a);
-	const double N = a / sqrt(1 - E * (sin(lat) * sin(lat)));
-	*x = (N + h) * cos(lat) * cos(lon);
-	*y = (N + h) * cos(lat) * sin(lon);
-	*z = ((1 - f) * (1 - f) * N + h) * sin(lat);
+	WGS84toECEF_rad(
+		x, y, z,
+		_geol_degtorad(lat),
+		_geol_degtorad(lon),
+		h
+	);
+}
+
+void WGS84toECEF_rad(
+	double* x,   double* y,   double* z,
+	double  lat, double  lon, double  h
+) {
+	const double N = _geol_a / sqrt(1 - _geol_E * sin(lat) * sin(lat));
+
+	*x = (h + N) * cos(lat) * cos(lon);
+	*y = (h + N) * cos(lat) * sin(lon);
+	*z = (h + N * (1 - _geol_E)) * sin(lat);
 }
 
 #endif
